043a084b5abe573f7cbb0a8339550896352806c0
Vladyslav Bilyi <beliyvladislav@gmail.com>
2019-01-03 13:18:53 +0200

    create extractor to retrieve data from request

---
+++ src/Extractor.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Bilyiv\RequestDataBundle;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * @author Vladyslav Bilyi <beliyvladislav@gmail.com>
+ */
+class Extractor implements ExtractorInterface
+{
+    /**
+     * @var array
+     */
+    public static $supportedFormats = ['json', 'xml', 'csv'];
+
+    /**
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * @var null|string
+     */
+    private $data;
+
+    /**
+     * @var null|string
+     */
+    private $format;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->request = $requestStack->getCurrentRequest();
+
+        $this->extractData();
+        $this->extractFormat();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getData(): ?string
+    {
+        return $this->data;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFormat(): ?string
+    {
+        return $this->format;
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSupportedFormats(): array
+    {
+        return self::$supportedFormats;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function addSupportedFormat(string $format)
+    {
+        self::$supportedFormats[] = $format;
+    }
+
+    /**
+     * Extract data from request.
+     */
+    protected function extractData()
+    {
+        switch ($this->request->getMethod()) {
+            case Request::METHOD_GET:
+                if ($query = $this->request->query->all()) {
+                    $this->data = \json_encode($query);
+                }
+                break;
+
+            case Request::METHOD_POST:
+            case Request::METHOD_PUT:
+            case Request::METHOD_PATCH:
+                $this->data = $this->request->getContent();
+                break;
+        }
+    }
+
+    /**
+     * Extract format from request.
+     */
+    protected function extractFormat()
+    {
+        if ($this->request->getMethod() === Request::METHOD_GET) {
+            $format = 'json';
+        } else {
+            $format = $this->request->getFormat($this->request->headers->get('content-type'));
+        }
+
+        if ($format && \in_array($format, static::$supportedFormats)) {
+            $this->format = $format;
+        }
+    }
+}
