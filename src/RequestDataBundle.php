46930d16772721301f9010c1c3f96374ba83c921
Vladyslav Bilyi <beliyvladislav@gmail.com>
2019-01-03 11:36:59 +0200

    create bundle php

---
+++ src/RequestDataBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Bilyiv\RequestDataBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Vladyslav Bilyi <beliyvladislav@gmail.com>
+ */
+class RequestDataBundle extends Bundle
+{
+}
