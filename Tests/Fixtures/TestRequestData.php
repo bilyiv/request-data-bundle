cf003c398db953f6434fc9b70b5203d94c0e5bd1
Vladyslav Bilyi <beliyvladislav@gmail.com>
2019-01-29 10:42:34 +0200

    commit TestRequestData fixture class

---
+++ Tests/Fixtures/TestRequestData.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bilyiv\RequestDataBundle\Tests\Fixtures;
+
+/**
+ * @author Vladyslav Bilyi <beliyvladislav@gmail.com>
+ */
+class TestRequestData
+{
+    /**
+     * @var null|string
+     */
+    public $foo;
+}
