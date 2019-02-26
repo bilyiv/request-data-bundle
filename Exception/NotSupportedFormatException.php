55ecbc70570c0b08bfc01defc17f9c95db36eef6
Vladyslav Bilyi <beliyvladislav@gmail.com>
2019-02-27 00:23:07 +0200

    create not supported format exception

---
+++ Exception/NotSupportedFormatException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Bilyiv\RequestDataBundle\Exception;
+
+/**
+ * @author Vladyslav Bilyi <beliyvladislav@gmail.com>
+ */
+class NotSupportedFormatException extends \Exception
+{
+}
